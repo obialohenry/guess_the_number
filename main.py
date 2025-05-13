ac47ec767e1a5304b39b94a3bebd58c299e62f07
sisu <obilaorchisomebi123@gmail.com>
2025-05-13 14:34:34 +0100

    imported logo ascii art in main.py from art.py created a number list variable ,and for loop to loop through numbers 1-100,adding each number to the list

---
+++ main.py
@@ -0,0 +1,10 @@
+from art import logo
+
+number_list = []
+for number in range(1,101):
+    number_list.append(number)
+print(f"number list: {number_list}")
+
+# def guess_the_number():
+#     print(logo)
+#     print("Welcome")
