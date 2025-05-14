0c4f3ce5a58a84994021b06a69234fd393e7e464
sisu <obilaorchisomebi123@gmail.com>
2025-05-14 10:45:53 +0100

    refator: The feedback method to just give feedback based on a players guessed number easy_or_hard_level method to check for when a players attempt is finished, and give feedback, and end the guessing loop.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from random import randint
 from art import logo
-import random
+
 
 EASY_LEVEL = 10
 HARD_LEVEL = 5
 
-number_list = []
-for number in range(1,101):
-    number_list.append(number)
-
-actual_number = random.choice(number_list)
+actual_number = randint(1,100)
 
 def attempt_word_form(level:int) -> str:
     """This method checks to see if a player have 1 attempt left,
@@ -20,45 +17,44 @@ def attempt_word_form(level:int) -> str:
     else:
         return "attempts"
 
-def feedback(guessed_number:int, level:int)->bool:
+def feedback(guessed_number:int)->bool:
     """This method gives feedback to the user based on a user inputted guessed number,
-       and if a user have run out of attempts. It returns a boolean value which is used
-       to continue or end the game.
+       and returns a boolean to tell the program whether to guess again or stop guessing.
     """
     if guessed_number == actual_number:
-        print(f"You got it! The answer was {actual_number}. Guess again.")
+        print(f"You got it! The answer was {actual_number}.")
         return False
-    else:
-        if level == 0:
-            print("You've run out of guesses, you lose.")
-            return False
-        elif guessed_number > actual_number:
-            print(f"Too high.\nGuess again")
-            return True
-        elif guessed_number < actual_number:
-            print(f"Too Low.\nGuess again")
-            return True
+    elif guessed_number > actual_number:
+        print("Too high.")
+        return True
+    elif guessed_number < actual_number:
+        print("Too Low.")
+        return True
+
 
 def easy_or_hard_level(level:int):
        """
        This method takes in an int datatype level, which stores the number of attempts
        according to the difficulty level a user picked, and it runs a loop according to
        the number of attempts, letting a user make a guess in attempt to get the actual
-       number.It ends the loop based on the feedback it gets from the "feedback" method.
+       number.It ends the loop based on the feedback it gets from the "feedback" method,
+       and if a player runs out of attempts.
        """
        guess_again = True
        while guess_again:
         print(f"You have {level} {attempt_word_form(level)} remaining to guess the number.")
         guessed_number = int(input("Make a guess: "))
         level -= 1
-        guess_again=feedback(guessed_number=guessed_number, level=level)
+        guess_again=feedback(guessed_number=guessed_number)
+        if level == 0:
+            print("You've run out of guesses, you lose.")
+            guess_again = False
+        else:
+            print("Guess again")
 
 
 
 def guess_the_number():
-    """
-    This is the number guessing game.
-    """
     print(logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
